1aa1235db45c26c9a050626733d6dd99136a8cd0
Duureeee <167115601+Duureeee@users.noreply.github.com>
2025-05-16 07:45:41 +0800

    1

--- Java/src/main/java/drawing/writing/JPEGWriter.java
+++ Java/src/main/java/drawing/writing/JPEGWriter.java
@@ -3,25 +3,30 @@ package drawing.writing;
 import java.io.IOException;
 import java.io.Writer;
 
-/**
- * Refactor Task 3: (Mis-)Shaped
- *
- * @author Zishen Wen (F22), Deyuan Chen (S22)
- */
 public class JPEGWriter extends Writer {
-    // This is a stub class. Pretend this writer work.
-    public JPEGWriter(String s) {
+    private final String filename;
+    private boolean closed = false;
+
+    public JPEGWriter(String filename) {
+        this.filename = filename;
+        System.out.println("JPEGWriter created for file: " + filename);
     }
 
     @Override
     public void write(char[] cbuf, int off, int len) throws IOException {
+        if (closed) throw new IOException("Writer is closed");
+        System.out.print(new String(cbuf, off, len));
     }
 
     @Override
     public void flush() throws IOException {
+        if (closed) throw new IOException("Writer is closed");
+        System.out.println("\nJPEGWriter flushed");
     }
 
     @Override
     public void close() throws IOException {
+        closed = true;
+        System.out.println("JPEGWriter closed for file: " + filename);
     }
 }
--- Java/src/main/java/drawing/writing/PNGWriter.java
+++ Java/src/main/java/drawing/writing/PNGWriter.java
@@ -3,25 +3,30 @@ package drawing.writing;
 import java.io.IOException;
 import java.io.Writer;
 
-/**
- * Refactor Task 3: (Mis-)Shaped
- *
- * @author Zishen Wen (F22), Deyuan Chen (S22)
- */
 public class PNGWriter extends Writer {
-    // This is a stub class. Pretend this writer work.
-    public PNGWriter(String s) {
+    private final String filename;
+    private boolean closed = false;
+
+    public PNGWriter(String filename) {
+        this.filename = filename;
+        System.out.println("PNGWriter created for file: " + filename);
     }
 
     @Override
     public void write(char[] cbuf, int off, int len) throws IOException {
+        if (closed) throw new IOException("Writer is closed");
+        System.out.print(new String(cbuf, off, len));
     }
 
     @Override
     public void flush() throws IOException {
+        if (closed) throw new IOException("Writer is closed");
+        System.out.println("\nPNGWriter flushed");
     }
 
     @Override
     public void close() throws IOException {
+        closed = true;
+        System.out.println("PNGWriter closed for file: " + filename);
     }
 }
