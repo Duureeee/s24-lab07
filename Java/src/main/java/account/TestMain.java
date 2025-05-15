6cbffe4ea36678cd19486e198677a870a6ba5f81
Duureeee <167115601+Duureeee@users.noreply.github.com>
2025-05-16 07:28:32 +0800

    a

--- Java/src/main/java/account/TestMain.java
+++ Java/src/main/java/account/TestMain.java
@@ -1,24 +1,24 @@
 package account;
 
-// Турших зориулалттай үндсэн класс
+import account.Student;
+import account.Loan;
+import account.DebitCard;
+ 
 public class TestMain {
     public static void main(String[] args) {
-        // Student-ийг үүсгэж үзэх
         Student student = new Student("Suldee", 2025001, 3.75);
         System.out.println("Student: " + student.getName() + ", ID: " + student.getStudentID() + ", GPA: " + student.getGPA());
 
-        // Loan данс үүсгэх
         Loan loan = new Loan("Suldee", 1001, 50000, 5000);
         System.out.println("Loan Balance: " + loan.getBalanceRemaining());
-        System.out.println("Pay loan 4000: " + loan.pay(4000));  // false, бага төлбөр
-        System.out.println("Pay loan 6000: " + loan.pay(6000));  // true
+        System.out.println("Pay loan 4000: " + loan.pay(4000));
+        System.out.println("Pay loan 6000: " + loan.pay(6000));
         System.out.println("Loan Balance after payment: " + loan.getBalanceRemaining());
 
-        // DebitCard үүсгэх
         DebitCard debitCard = new DebitCard("Suldee", 2001, 10000);
         System.out.println("DebitCard Balance: " + debitCard.getBalance());
-        System.out.println("Pay debit 12000: " + debitCard.pay(12000));  // false, хэтэрсэн
-        System.out.println("Pay debit 7000: " + debitCard.pay(7000));    // true
+        System.out.println("Pay debit 12000: " + debitCard.pay(12000));
+        System.out.println("Pay debit 7000: " + debitCard.pay(7000));
         System.out.println("DebitCard Balance after payment: " + debitCard.getBalance());
     }
 }
